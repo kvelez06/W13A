6f0bce93bfb55e22868660ff726e4c9988a5fa48
Karl <heroware.06@hotmail.com>
2020-09-17 05:12:39 -0600

    done w/o bonus

---
+++ vuetesting/src/components/body.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="body-grid">
+        <div></div>
+        <h2>Side-Panel</h2>
+        <h2>Meet the Dev</h2>
+        <h2>Meet the Team</h2>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: "page-body"
+    }
+</script>
+
+
+<style scoped>
+    .body-grid {
+        display: grid;
+        justify-items: center;
+        align-items: center;
+        grid-template-columns: 1fr 2fr 1fr 1fr 1fr;
+    }
+    
+</style>
